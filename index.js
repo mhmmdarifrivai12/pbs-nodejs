3a30cd038ccec17657b40a5ea9fd10daa29fd99d
mhmmdarifrivai12 <mhmmdarifrivai12@gmail.com>
2024-04-01 13:31:54 +0700

    Menambahkan metode PUT dan DELETE untuk CRUD

--- index.js
+++ index.js
@@ -45,6 +45,39 @@ app.post('/produk', (req, res) => {
     });
 });
 
+// Update data produk (PUT)
+app.put('/produk/:id', (req, res) => {
+    const id_produk = req.params.id;
+    const { nama_produk, harga, stok } = req.body;
+    const values = { nama_produk, harga, stok };
+
+    db.query('UPDATE produk SET ? WHERE id_produk = ?', [values, id_produk], (error, result) => {
+        if (error) {
+            console.error(error);
+            res.status(500).send("Gagal memperbarui data produk.");
+        } else {
+            res.json({
+                message: "Data produk berhasil diperbarui."
+            });
+        }
+    });
+});
+//delete produk
+app.delete('/produk/:id', (req, res) => {
+    const id_produk = req.params.id;
+
+    db.query('DELETE FROM produk WHERE id_produk = ?', id_produk, (error, result) => {
+        if (error) {
+            console.error(error);
+            res.status(500).send("Gagal menghapus data produk.");
+        } else {
+            res.json({
+                message: "Data produk berhasil dihapus."
+            });
+        }
+    });
+});
+
 // Get data Pelanggan
 app.get('/pelanggan', (req, res) => {
     db.query('SELECT * FROM Pelanggan', (error, results) => { 
@@ -77,6 +110,40 @@ app.post('/pelanggan', (req, res) => {
                         data: results[0] // Mengirimkan detail data yang baru ditambahkan
                     });
                 }
+            });
+        }
+    });
+});
+
+// Update data pelanggan (PUT)
+app.put('/pelanggan/:id', (req, res) => {
+    const id_pelanggan = req.params.id;
+    const { nama_pelanggan, alamat, email } = req.body;
+    const values = { nama_pelanggan, alamat, email };
+
+    db.query('UPDATE pelanggan SET ? WHERE id_pelanggan = ?', [values, id_pelanggan], (error, result) => {
+        if (error) {
+            console.error(error);
+            res.status(500).send("Gagal memperbarui data pelanggan.");
+        } else {
+            res.json({
+                message: "Data pelanggan berhasil diperbarui."
+            });
+        }
+    });
+});
+
+// Hapus data pelanggan (DELETE)
+app.delete('/pelanggan/:id', (req, res) => {
+    const id_pelanggan = req.params.id;
+
+    db.query('DELETE FROM pelanggan WHERE id_pelanggan = ?', id_pelanggan, (error, result) => {
+        if (error) {
+            console.error(error);
+            res.status(500).send("Gagal menghapus data pelanggan.");
+        } else {
+            res.json({
+                message: "Data pelanggan berhasil dihapus."
             });
         }
     });
@@ -119,6 +186,38 @@ app.post('/transaksi', (req, res) => {
     });
 });
 
+// Update data transaksi (PUT)
+app.put('/transaksi/:id', (req, res) => {
+    const id_transaksi = req.params.id;
+    const { id_pelanggan, tanggal_transaksi, total } = req.body;
+    const values = { id_pelanggan, tanggal_transaksi, total };
+
+    db.query('UPDATE transaksi SET ? WHERE id_transaksi = ?', [values, id_transaksi], (error, result) => {
+        if (error) {
+            console.error(error);
+            res.status(500).send("Gagal memperbarui data transaksi.");
+        } else {
+            res.json({
+                message: "Data transaksi berhasil diperbarui."
+            });
+        }
+    });
+});
+// Hapus data transaksi (DELETE)
+app.delete('/transaksi/:id', (req, res) => {
+    const id_transaksi = req.params.id;
+
+    db.query('DELETE FROM transaksi WHERE id_transaksi = ?', id_transaksi, (error, result) => {
+        if (error) {
+            console.error(error);
+            res.status(500).send("Gagal menghapus data transaksi.");
+        } else {
+            res.json({
+                message: "Data transaksi berhasil dihapus."
+            });
+        }
+    });
+});
 
 // GET data Detail Transaksi (GET)
 app.get('/detail_transaksi', (req, res) => {
@@ -152,6 +251,40 @@ app.post('/detail_transaksi', (req, res) => {
                         data: results[0] // Mengirimkan detail data yang baru ditambahkan
                     });
                 }
+            });
+        }
+    });
+});
+
+// Update data detail transaksi (PUT)
+app.put('/detail_transaksi/:id', (req, res) => {
+    const id_detail_transaksi = req.params.id;
+    const { id_transaksi, id_produk, jumlah, subtotal } = req.body;
+    const values = { id_transaksi, id_produk, jumlah, subtotal };
+
+    db.query('UPDATE detail_transaksi SET ? WHERE id_detail_transaksi = ?', [values, id_detail_transaksi], (error, result) => {
+        if (error) {
+            console.error(error);
+            res.status(500).send("Gagal memperbarui data detail transaksi.");
+        } else {
+            res.json({
+                message: "Data detail transaksi berhasil diperbarui."
+            });
+        }
+    });
+});
+
+// Hapus data detail transaksi (DELETE)
+app.delete('/detail_transaksi/:id', (req, res) => {
+    const id_detail_transaksi = req.params.id;
+
+    db.query('DELETE FROM detail_transaksi WHERE id_detail_transaksi = ?', id_detail_transaksi, (error, result) => {
+        if (error) {
+            console.error(error);
+            res.status(500).send("Gagal menghapus data detail transaksi.");
+        } else {
+            res.json({
+                message: "Data detail transaksi berhasil dihapus."
             });
         }
     });
